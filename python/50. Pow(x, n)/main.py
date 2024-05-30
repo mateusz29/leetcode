b79bccbc814d6fb2859cc05b7db3f4dde1a87e7b
mateusz29 <strzeleckimateusz10@gmail.com>
2024-05-30 14:15:52 +0200

    Pow(x, n)

---
+++ python/50. Pow(x, n)/main.py
@@ -0,0 +1,14 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1
+        if n == -1:
+            return 1/x
+        if n == 1:
+            return x
+
+        return self.myPow(x*x, n//2) * self.myPow(x, n % 2)
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.myPow(2, 6))
