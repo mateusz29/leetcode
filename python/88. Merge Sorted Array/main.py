e051c27fa46ef5119b0b06cabb184baf928fa7fa
mateusz29 <strzeleckimateusz10@gmail.com>
2024-05-31 21:58:07 +0200

    Merge Sorted Array

---
+++ python/88. Merge Sorted Array/main.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        for i in range(n):
+            nums1[m+i] = nums2[i]
+        
+        nums1.sort()
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.merge([1,2,3,0,0,0],3,[2,5,6],3))
