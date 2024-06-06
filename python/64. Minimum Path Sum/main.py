5db1a3fb91eef975fd33256a0160c8902e76a77d
mateusz29 <strzeleckimateusz10@gmail.com>
2024-06-06 19:27:46 +0200

    Minimum Path Sum

---
+++ python/64. Minimum Path Sum/main.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        m, n  = len(grid), len(grid[0])
+
+        for i in range(1, m):
+            grid[i][0] += grid[i-1][0]
+            
+        for j in range(1, n):
+            grid[0][j] += grid[0][j-1]
+
+        for i in range(1, m):
+            for j in range(1, n):
+                grid[i][j] += min(grid[i-1][j], grid[i][j-1])
+        
+        return grid[m-1][n-1]
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.minPathSum([[1,2,3],[4,5,6]]))
