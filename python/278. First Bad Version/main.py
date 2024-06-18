5a60d1314b92f7444bee41c48b6d9c73c3e5bf23
mateusz29 <strzeleckimateusz10@gmail.com>
2024-06-18 16:28:20 +0200

    First Bad Version python new

--- python/278. First Bad Version/main.py
+++ python/278. First Bad Version/main.py
@@ -6,15 +6,13 @@ class Solution:
     def firstBadVersion(self, n: int) -> int:
         start = 1
         end = n
-        num = 0
-        while start <= end:
+        while start < end:
             mid = start + (end - start) // 2
             if isBadVersion(mid):
-                num = mid
-                end = mid - 1
+                end = mid
             else:
                 start = mid + 1
-        return num
+        return start
 
 if __name__ == "__main__":
     solution = Solution()
