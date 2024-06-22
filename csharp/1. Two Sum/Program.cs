e9cadda177a3fa881b950aad8cc678eab5c15bce
mateusz29 <strzeleckimateusz10@gmail.com>
2024-06-22 17:45:40 +0200

    Two Sum and Palindrome Number C#

---
+++ csharp/1. Two Sum/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int secondNum = target - nums[i];
+            if (dict.ContainsKey(secondNum))
+            {
+                return new int[] { dict[secondNum], i };
+            }
+            if (!dict.ContainsKey(nums[i]))
+            {
+                dict.Add(nums[i], i);
+            }
+        }
+        return new int[] { -1, -1 };
+    }
+
+    public static void Main(string[] args)
+    {
+        int[] nums = new int[] { 3,3 };
+        int target = 6;
+        Solution s = new Solution();
+        int[] result = s.TwoSum(nums, target);
+        Console.WriteLine(result[0] + " " + result[1]);
+    }
+}
---
+++ csharp/9. Palindrome Number/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Solution {
+    public bool IsPalindrome(int x) {
+        if (x < 0)
+            return false;
+        else
+        {
+            int tmpx = x;
+            int reverse = 0;
+            while (x != 0)
+            {
+                int digit =tmpx % 10;
+                tmpx /= 10;
+                reverse = reverse * 10 + digit;
+            }
+            return reverse == x;
+        }        
+    }
+
+    public static void Main(string[] args)
+    {
+        int x = 121;
+        Solution s = new Solution();
+        Console.WriteLine(s.IsPalindrome(x));
+    }
+}
