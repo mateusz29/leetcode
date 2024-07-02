b7f1a03a62f3cac96329353537e2dddcf95e591d
mateusz29 <strzeleckimateusz10@gmail.com>
2024-07-02 19:13:56 +0200

    small refactoring

---
+++ python/pandas/main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from typing import List
+
+def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
+    return pd.DataFrame(student_data, columns=['student_id', 'age'])
+
+def getDataframeSize(players: pd.DataFrame) -> List[int]:
+    return [players.shape[0], players.shape[1]]
+
+df = createDataframe([[1,15],[2,11],[3,11],[4,20]])
+print(df)
+
+size = getDataframeSize(df)
+print(size)
