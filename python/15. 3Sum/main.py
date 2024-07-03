0cc0a9ad0cbbe3c547e6bb5ac05aeca8a70a2eb4
mateusz29 <strzeleckimateusz10@gmail.com>
2024-07-03 20:03:16 +0200

    3Sum python fixed

--- python/15. 3Sum/main.py
+++ python/15. 3Sum/main.py
@@ -2,20 +2,28 @@ from typing import List
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
         n = len(nums)
-
         triplets = []
-        for i in range(n-2):
-            for j in range(i+1,n-1):
-                for k in range(j+1,n):
-                    print(f"{i} {j} {k}")
-                    if nums[i] + nums[j] + nums[k] == 0:
-                        triplet = [nums[i],nums[j],nums[k]]
-                        triplet.sort()
-                        if triplet not in triplets:
-                            triplets.append(triplet)
 
-        return triplets                        
+        for i in range(n):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+
+            left, right = i + 1, n - 1
+            while left < right:
+                cur_sum = nums[i] + nums[left] + nums[right]
+                if cur_sum < 0:
+                    left += 1
+                elif cur_sum > 0:
+                    right -= 1
+                else:
+                    triplets.append([nums[i], nums[left], nums[right]])
+                    left += 1
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    
+        return triplets
 
 if __name__ == "__main__":   
     solution = Solution()
