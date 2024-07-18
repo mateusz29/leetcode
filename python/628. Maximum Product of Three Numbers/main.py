ec5102007b25b46cfe667ada1867e87e13f32901
mateusz29 <strzeleckimateusz10@gmail.com>
2024-07-19 00:22:19 +0200

    Maximum Product of Three Numbers python

---
+++ python/628. Maximum Product of Three Numbers/main.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def maximumProduct(self, nums: List[int]) -> int:
+        nums.sort()
+        return max(nums[-1] * nums[-2] * nums[-3], nums[0] * nums[1] * nums[-1])
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.maximumProduct([-1,-2,-3,-4,-5]))
