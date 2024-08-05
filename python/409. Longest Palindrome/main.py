ab4dbf58b74987ba1c6e5ca1880433d92769652d
mateusz29 <strzeleckimateusz10@gmail.com>
2024-08-05 17:27:38 +0200

    Longest Palindrome python

---
+++ python/409. Longest Palindrome/main.py
@@ -0,0 +1,24 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        freqs = {}
+        for char in s:
+            freqs[char] = freqs.get(char, 0) + 1
+        
+        length = 0
+        odd = False
+        for value in freqs.values():
+            if value % 2 == 0:
+                length += value
+            else:
+                odd = True
+                if value != 1:
+                    length += value - 1
+        
+        if odd:
+            return length + 1
+        else:
+            return length
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.longestPalindrome("civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth"))
