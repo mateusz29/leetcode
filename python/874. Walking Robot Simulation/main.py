793d6095e1ee9c31383706b60230995e611f6176
mateusz29 <strzeleckimateusz10@gmail.com>
2024-09-04 16:07:54 +0200

    Walking Robot Simulation python, updated solution

--- python/874. Walking Robot Simulation/main.py
+++ python/874. Walking Robot Simulation/main.py
@@ -1,50 +1,23 @@
 class Solution:
     def robotSim(self, commands: list[int], obstacles: list[list[int]]) -> int:
-        dx, dy = 0, 1
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        dir_idx = 0
         x, y = 0, 0
         max_distance = 0
-        print(len(obstacles))
         obstacles = set(map(tuple, obstacles))
-        print(len(obstacles))
+
         for command in commands:
-            # left
-            if command == -2:
-                if dx == 0 and dy == 1:
-                    dx = -1
-                    dy = 0
-                elif dx == -1 and dy == 0:
-                    dx = 0
-                    dy = -1
-                elif dx == 0 and dy == -1:
-                    dx = 1
-                    dy = 0
-                else:
-                    dx = 0
-                    dy = 1
-            # right
-            elif command == -1:
-                if dx == 0 and dy == 1:
-                    dx = 1
-                    dy = 0
-                elif dx == 1 and dy == 0:
-                    dx = 0
-                    dy = -1
-                elif dx == 0 and dy == -1:
-                    dx = -1
-                    dy = 0
-                else:
-                    dx = 0
-                    dy = 1
-            # forward
-            else:
+            if command == -2:  # left
+                dir_idx = (dir_idx - 1) % 4
+            elif command == -1:  # right
+                dir_idx = (dir_idx + 1) % 4
+            else:  # forward
                 for _ in range(command):
-                    x += dx
-                    y += dy
-                    if (x, y) in obstacles:
-                        x -= dx
-                        y -= dy
-                        break
-                    max_distance = max(max_distance, x**2 + y**2)
+                    dx, dy = directions[dir_idx]
+                    if (x + dx, y + dy) not in obstacles:
+                        x += dx
+                        y += dy
+                        max_distance = max(max_distance, x**2 + y**2)
 
         return max_distance
 
